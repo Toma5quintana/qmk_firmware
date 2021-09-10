26dc423259bacfd86ae7ae7b21de674204722acc
Glory <gloryhzw@gmail.com>
2021-09-10 23:00:37 +0800

    Make qmk flash work on SN32
    By using a command line sonix flasher (https://github.com/SonixQMK/sonix-flasher/pull/10), we can use qmk flash directly now by adding DFU_UTIL in rules.mk.
    
    Example: DFU_UTIL=sonix-flasher/sonix-flasher.exe 0c45 7010 200

--- tmk_core/chibios.mk
+++ tmk_core/chibios.mk
@@ -386,6 +386,10 @@ define EXEC_TEENSY
 	$(TEENSY_LOADER_CLI) -mmcu=$(MCU_LDSCRIPT) -w -v $(BUILD_DIR)/$(TARGET).hex
 endef
 
+define EXEC_SONIX_FLASHER
+	$(DFU_UTIL) $(DFU_ARGS) $(BUILD_DIR)/$(TARGET).bin
+endef
+
 teensy: $(BUILD_DIR)/$(TARGET).hex cpfirmware sizeafter
 	$(call EXEC_TEENSY)
 
@@ -402,7 +406,7 @@ else ifeq ($(strip $(MCU_FAMILY)),KINETIS)
 else ifeq ($(strip $(MCU_FAMILY)),STM32)
 	$(call EXEC_DFU_UTIL)
 else ifeq ($(strip $(MCU_FAMILY)),SN32)
-	$(call EXEC_DFU_UTIL)
+	$(call EXEC_SONIX_FLASHER)
 else
 	$(PRINT_OK); $(SILENT) || printf "$(MSG_FLASH_BOOTLOADER)"
 endif
