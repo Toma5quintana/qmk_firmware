74348cdacc0fcf97b9476a64967558cada270b5c
Simone Gaiarin <simgunz@gmail.com>
2021-09-30 08:25:00 +0200

    Stop processing after processing custom keycode

--- keyboards/keychron/c1/keymaps/default/keymap.c
+++ keyboards/keychron/c1/keymaps/default/keymap.c
@@ -121,6 +121,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case RST_KB:
             NVIC_SystemReset();
+            return false;
     }
     return true;
 };
