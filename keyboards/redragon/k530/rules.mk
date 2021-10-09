238e5befc0ab20da006893a7ad04db9b8010ed84
Johns <johns98@gmx.net>
2021-10-09 13:25:44 +0200

    Improved k530 default keymap.
    Added cursor keys to jikl.
    Added home/end keys to qe and uo.
    Added dual role cursor keys to FN,SHIFT,APP,CTRL, tapping gives
    cursor keys, holding the old function. Tap hold repeats cursor
    keys.
    Added RGB controls to rtz fgh \.
    Added game layer with GUI disabled and CAPS without dual role.
    FN+M enables game layer, FN+, disables it.

--- keyboards/redragon/k530/rules.mk
+++ keyboards/redragon/k530/rules.mk
@@ -48,6 +48,7 @@ MOUSEKEY_ENABLE = no    # Mouse keys
 EXTRAKEY_ENABLE = yes   # Audio control and System control
 CONSOLE_ENABLE = no     # Console for debug
 COMMAND_ENABLE = no     # Commands for debug and configuration
+TAP_DANCE_ENABLE = yes  # Enable TapDance functionality
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no        # USB Nkey Rollover
 AUDIO_ENABLE = no
