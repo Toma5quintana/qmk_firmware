5e66048c1a6ff0914760bb9d6fbabda0dd028fcc
Jack <jackthughes@yahoo.com>
2021-11-29 00:23:01 -0800

    Remove "bin/"

--- bin/build_all.py
+++ bin/build_all.py
@@ -32,7 +32,7 @@ BOARDS = [
 
 error = False
 for kb in BOARDS:
-    if subprocess.run(f"bin/qmk compile -kb {kb} -km all -j{os.cpu_count()}", shell=True).returncode != 0:
+    if subprocess.run(f"qmk compile -kb {kb} -km all -j{os.cpu_count()}", shell=True).returncode != 0:
         error = True
 if error:
     sys.exit(1)
