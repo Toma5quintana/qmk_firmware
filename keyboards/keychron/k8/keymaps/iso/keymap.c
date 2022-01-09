d7de9144c9858423ea2272d30de04c444f697ce0
Thomas Bowman Mørch <thomas.git@bowmo.dk>
2022-01-09 17:46:59 +0100

    Swap volume up/down

--- keyboards/keychron/k8/keymaps/iso/keymap.c
+++ keyboards/keychron/k8/keymaps/iso/keymap.c
@@ -19,7 +19,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
-/*				+--------------------------------------------------------------------------+----------------+
+/*			+--------------------------------------------------------------------------+----------------+
 				| ESC |  | F1 | F2 | F3 | F4 | | F5 | F6 | F7 | F8 | | F9| F10| F11| F12|  | |PSCR|????|PAUS|
 				+--------------------------------------------------------------------------+------|----|----|
  				|  ~  |  1 |  2 |  3 |  4 |  5 |  6 |  7 |  8 |  9 |  0 |  - |  = | BACKSP | |INS |SCRL|PGUP|
@@ -39,11 +39,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                 {  KC_TAB,     KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN},
                 {  KC_CAPS,    KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,    KC_NO,     KC_NO,     KC_NO},
                 {  KC_LSFT,    KC_NUBS,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_NO,     KC_UP,     KC_NO},
-                {  KC_LCTL,    KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   KC_RGUI,   MO(1),     KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT}
+                {  KC_LCTL,    KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   KC_RGUI,   LT(1, KC_APP), KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT}
               },
 
     /*  Row:       0        1                   2                 3        4               5        6        7        8        9        10        11       12         13         14                15        16        */
-    [1] =   {   {    RESET, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_APP,  KC_MY_COMPUTER, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC__MUTE, KC_VOLU, KC_VOLD, KC_NO,   KC_PSCR,          KC_SLEP, RGB_TOG },
+    [1] =   {   {    RESET, KC_BRIGHTNESS_DOWN, KC_BRIGHTNESS_UP, KC_APP,  KC_MY_COMPUTER, RGB_VAD, RGB_VAI, KC_MPRV, KC_MPLY, KC_MNXT, KC__MUTE, KC_VOLD, KC_VOLU, KC_NO,   KC_PSCR,          KC_SLEP, RGB_TOG },
                 {  _______, _______,            _______,          _______, _______,        _______, _______, _______, _______, _______, _______,  _______, _______, _______, RGB_SAI,          RGB_SAI, RGB_HUI },
                 {  _______, _______,            _______,          _______, _______,        _______, _______, _______, _______, _______, _______,  _______, _______, _______, RGB_SAD,          RGB_SAD, RGB_HUD },
                 {  _______, _______,            _______,          _______, _______,        _______, _______, _______, _______, _______, _______,  _______, _______, _______, KC_NO,            KC_NO,   KC_NO },
