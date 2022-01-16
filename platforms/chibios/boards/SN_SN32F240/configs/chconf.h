82226420f025f1a19b90ac3dfc9af0c70d9dd5df
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-16 11:31:22 +0200

    sn32: 24x/B: enable MEMPOOLS

--- platforms/chibios/boards/SN_SN32F240/configs/chconf.h
+++ platforms/chibios/boards/SN_SN32F240/configs/chconf.h
@@ -378,7 +378,7 @@
  * @note    The default is @p TRUE.
  */
 #if !defined(CH_CFG_USE_MEMPOOLS)
-#define CH_CFG_USE_MEMPOOLS                 FALSE
+#define CH_CFG_USE_MEMPOOLS                 TRUE
 #endif
 
 /**
