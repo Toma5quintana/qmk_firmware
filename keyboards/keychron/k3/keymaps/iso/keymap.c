323fb1a2f520df01ea683b600bd60b94ce7e15f7
Gibbio <gibbio@gmail.com>
2022-01-16 10:44:16 +0100

    Fixed typo

--- keyboards/keychron/k3/keymaps/iso/keymap.c
+++ keyboards/keychron/k3/keymaps/iso/keymap.c
@@ -23,8 +23,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 enum layer_names {
   MAC_BASE = 0,
   WIN_BASE,
-  MAC_FM,
-  WIN_FM
+  MAC_FN,
+  WIN_FN
 };
 
 #define KC_TASK LGUI(KC_TAB)        // Task viewer
@@ -60,7 +60,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_NO,     KC_NO,     KC_PGDN   },
     { KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,    KC_NO,     KC_HOME   },
     { KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_END    },
-    { KC_LCTL,   KC_LALT,   KC_LGUI,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(MAC_FM),KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT   }
+    { KC_LCTL,   KC_LALT,   KC_LGUI,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(MAC_FN),KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT   }
   },
 
 /*  Mac Fn overlay
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |     |     |     |                             |     |    |    |RHUD|RSAD|RHUI|
     +------------------------------------------------------------------------------+
 */
-  [MAC_FM] = {
+  [MAC_FN] = {
   /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15         */
     { RESET,      KC_BRID,    KC_BRIU,    KC_MSSN,    KC_FIND,    RGB_VAD,    RGB_VAI,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_MSNP,    KC_NO,      RGB_TOG    },
     { KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,    RGB_M_P    },
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     { KC_TAB,    KC_Q,      KC_W,      KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_NO,     KC_NO,     KC_PGDN   },
     { KC_CAPS,   KC_A,      KC_S,      KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,      KC_SCLN,   KC_QUOT,   KC_NUHS,   KC_ENT,    KC_NO,     KC_HOME   },
     { KC_LSFT,   KC_NUBS,   KC_Z,      KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,   KC_DOT,    KC_SLSH,   KC_NO,     KC_RSFT,   KC_UP,     KC_END    },
-    { KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(WIN_FM),KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT   }
+    { KC_LCTL,   KC_LGUI,   KC_LALT,   KC_NO,   KC_NO,   KC_NO,   KC_SPC,   KC_NO,   KC_NO,   KC_NO,     KC_RALT,   MO(WIN_FN),KC_RCTL,   KC_LEFT,   KC_DOWN,   KC_RGHT   }
   },
 
 /*  Windows Fn overlay
@@ -128,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     |     |     |     |                             |     |    |    |RHUD|RSAD|RHUI|
     +------------------------------------------------------------------------------+
 */
-  [WIN_FM] = {
+  [WIN_FN] = {
   /*  0           1           2           3           4           5           6           7           8           9           10          11          12          13          14          15         */
     { RESET,      KC_BRID,    KC_BRIU,    KC_TASK,    KC_FLXP,    RGB_VAD,    RGB_VAI,    KC_MPRV,    KC_MPLY,    KC_MNXT,    KC_MUTE,    KC_VOLD,    KC_VOLU,    KC_SNIP,    KC_NO,      RGB_TOG    },
     { KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      KC_NO,      XXXXXXX,    RGB_M_P    },
