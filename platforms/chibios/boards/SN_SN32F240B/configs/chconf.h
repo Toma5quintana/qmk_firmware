9ee652b389682552d46f7c66b39f5d90f85ae926
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-20 15:33:40 +0200

    testing: back to hw systick

--- platforms/chibios/boards/SN_SN32F240B/configs/chconf.h
+++ platforms/chibios/boards/SN_SN32F240B/configs/chconf.h
@@ -43,7 +43,7 @@
  * @note    Allowed values are 16, 32 or 64 bits.
  */
 #if !defined(CH_CFG_ST_RESOLUTION)
-#define CH_CFG_ST_RESOLUTION                16
+#define CH_CFG_ST_RESOLUTION                32
 #endif
 
 /**
@@ -80,7 +80,7 @@
  *          this value.
  */
 #if !defined(CH_CFG_ST_TIMEDELTA)
-#define CH_CFG_ST_TIMEDELTA                 2
+#define CH_CFG_ST_TIMEDELTA                 0
 #endif
 
 /** @} */
