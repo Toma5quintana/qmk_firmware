48456575875b3426994857d8210325426d5cc657
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-22 18:53:05 +0200

    Revert "remove this for now"
    This reverts commit f344030ea531710b3988f8137c4ce42676fbd864.

--- tmk_core/protocol/chibios/usb_main.c
+++ tmk_core/protocol/chibios/usb_main.c
@@ -738,7 +738,7 @@ void init_usb_driver(USBDriver *usbp) {
      * after a reset.
      */
     usbDisconnectBus(usbp);
-    wait_ms(100);
+    wait_ms(1500);
     usbStart(usbp, &usbcfg);
     usbConnectBus(usbp);
 
