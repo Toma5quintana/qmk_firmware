ee54f48ec038a0ad6e9f1f619e41102212949f6d
Dimitris Mantzouranis <d3xter93@gmail.com>
2022-01-24 02:50:55 +0200

    how did this get in here

--- keyboards/redragon/k580/config_led.h
+++ keyboards/redragon/k580/config_led.h
@@ -28,4 +28,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define LED_MATRIX_COLS MATRIX_COLS
 #define LED_MATRIX_COL_PINS MATRIX_COL_PINS
 
-#define DRIVER_LED_TOTAL (124)
+#define DRIVER_LED_TOTAL (118)
