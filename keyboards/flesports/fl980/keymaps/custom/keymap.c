505720fb4423e51b59e4c00ec45c701659a9a558
GloooM <glooom@xtlt.ru>
2022-11-16 00:54:05 +0500

    custom layout

--- keyboards/flesports/fl980/keymaps/custom/keymap.c
+++ keyboards/flesports/fl980/keymaps/custom/keymap.c
@@ -25,9 +25,20 @@ enum layer_names {
     _FN,
 };
 
+enum {
+  TD_HOME_PGUP = 0,
+  TD_END_PGDN = 1
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+  [TD_HOME_PGUP]  = ACTION_TAP_DANCE_DOUBLE(KC_HOME, KC_PGUP),
+  [TD_END_PGDN]  = ACTION_TAP_DANCE_DOUBLE(KC_END, KC_PGDN),
+};
+
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*  Row:        0           1           2           3        4           5        6        7           8           9           10          11          12          13          14          15          16          17      */
-    [_BASE] = { {   KC_ESC,     KC_F1,      KC_F2,      KC_F3,   KC_F4,      KC_F5,   KC_F6,   KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_NO,      KC_HOME,   KC_END,    KC_INS,     KC_PSCR },
+    [_BASE] = { {   KC_ESC,     KC_F1,      KC_F2,      KC_F3,   KC_F4,      KC_F5,   KC_F6,   KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_NO,      TD(TD_HOME_PGUP),   TD(TD_END_PGDN),    KC_INS,     KC_PSCR },
                 {   KC_GRV,     KC_1,       KC_2,       KC_3,    KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,    KC_DEL,    KC_PSLS,   KC_PAST,    KC_PMNS },
                 {   KC_TAB,     KC_Q,       KC_W,       KC_E,    KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,    KC_P7,     KC_P8,     KC_P9,      KC_PPLS },
                 {   KC_CAPS,    KC_A,       KC_S,       KC_D,    KC_F,       KC_G,    KC_H,    KC_J,       KC_K,       KC_L,       KC_SCLN,    KC_QUOT,    KC_NO,      KC_ENT,     KC_P4,     KC_P5,     KC_P6,      KC_PENT },
