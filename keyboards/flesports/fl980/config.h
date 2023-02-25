4e64f5d388a37031dcb4c84569e63328253f8a21
GloooM <xglooom@gmail.com>
2023-02-25 16:19:31 +0500

    update license headers

--- keyboards/flesports/fl980/config.h
+++ keyboards/flesports/fl980/config.h
@@ -1,10 +1,10 @@
-/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
- *
+/* Copyright (C) 2023 Alexey Zagorodnikov <xglooom@gmail.com>
+ * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
- *
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
