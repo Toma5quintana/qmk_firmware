65f80b370f7209756360aab2ea742d0decd59838
Alexandre Oliveira <rockytvbr@gmail.com>
2023-08-05 18:38:30 +0200

    [WIP] Add Redragon K556

---
+++ keyboards/redragon/k556/config.h
@@ -0,0 +1,29 @@
+/* Copyright 2020 Adam Honse <calcprogrammer1@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+/* key matrix size */
+#define RGB_MATRIX_LED_COUNT 106
+
+#define RGB_MATRIX_ROW_PINS { C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+
+/* Enable additional RGB effects                                       */
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
+
+/* Configure the effects:                                              */
+#define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
