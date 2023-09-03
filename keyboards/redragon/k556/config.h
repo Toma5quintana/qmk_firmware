71154956bcb1ed6cd793c6d4758d74b16ee2cd2e
Alexandre Oliveira <rockytvbr@gmail.com>
2023-09-03 10:42:18 +0200

    Fix wrong transistor config for RGB matrix

--- keyboards/redragon/k556/config.h
+++ keyboards/redragon/k556/config.h
@@ -19,7 +19,7 @@
 /* key matrix size */
 #define RGB_MATRIX_LED_COUNT 106
 
-#define RGB_MATRIX_ROW_PINS { C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
+#define RGB_MATRIX_ROW_PINS { C0, C1, C3, C4, C5, C6, C7, C8, C9, C10, C11, C12, B6, B7, B8, B9, B10, B11 }
 
 /* Enable additional RGB effects                                       */
 #define RGB_MATRIX_FRAMEBUFFER_EFFECTS
@@ -27,3 +27,7 @@
 
 /* Configure the effects:                                              */
 #define RGB_MATRIX_TYPING_HEATMAP_DECREASE_DELAY_MS 50
+
+/* Configure transistor logic for RGB matrix */
+#define PWM_OUTPUT_ACTIVE_LEVEL PWM_OUTPUT_ACTIVE_HIGH
+#define RGB_OUTPUT_ACTIVE_LEVEL RGB_OUTPUT_ACTIVE_LOW
