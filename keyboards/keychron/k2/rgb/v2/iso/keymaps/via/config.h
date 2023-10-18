647521ab14ca62d82780e725fcc48747c07a3293
Mika Salmela <mtsalmel84@gmail.com>
2023-10-18 15:14:07 +0300

    Update Keychron K2v2 RGB iso
    removed keyboards/keychron/k2/rgb/v2/iso/keymaps/via/config.h

--- keyboards/keychron/k2/rgb/v2/iso/keymaps/via/config.h
+++
@@ -1,8 +0,0 @@
-// Copyright 2023 KeijoMika (https://github.com/KeijoMika)
-// SPDX-License-Identifier: GPL-2.0-or-later
-
-#pragma once
-
-#define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define VIA_QMK_RGBLIGHT_ENABLE
-
