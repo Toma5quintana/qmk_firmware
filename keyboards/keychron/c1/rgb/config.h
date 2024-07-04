d979d0553d10064b0a38520582c544b2013a5d67
NetUserGet <22157020+NetUserGet@users.noreply.github.com>
2024-07-04 15:21:19 -0400

    Keychron C1 RGB: Patch timetravel.

--- keyboards/keychron/c1/rgb/config.h
+++ keyboards/keychron/c1/rgb/config.h
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 NetUserGet
+Copyright 2024 NetUserGet
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
