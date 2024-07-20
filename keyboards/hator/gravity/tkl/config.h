7707f5a6a304263ff29a257f65e22b815494d206
Matros1975 <khrustaleu.andrei@gmail.com>
2024-07-20 15:04:15 +0200

    license adjusted

--- keyboards/hator/gravity/tkl/config.h
+++ keyboards/hator/gravity/tkl/config.h
@@ -1,7 +1,7 @@
-// Copyright 2023 Alex (@AlexHtr)
 // SPDX-License-Identifier: GPL-2.0-or-later
-
-/* This program is free software: you can redistribute it and/or modify
+/* Copyright (C) 2024 Hator 
+ * 
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
@@ -13,7 +13,8 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
+
 
 #pragma once
 
